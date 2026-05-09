cd75c800b5cd077ae9af3a3ef0854f89891e4bec
tarek-moh <tarekmoh5557@gmail.com>
2026-05-09 22:31:43 +0300

    add bias-variance analysis for the SVM weston watkins model

--- models/svm-ww/model.py
+++ models/svm-ww/model.py
@@ -2,10 +2,11 @@ import numpy as np
 import pandas as pd
 
 class SVM_Multiclass:
-    def __init__(self, x_train: pd.DataFrame, y_train: pd.DataFrame):
-        self.x_train = x_train.to_numpy()
-        self.x_train = np.column_stack((self.x_train, np.ones(self.x_train.shape[0]))) 
-        self.y_train = y_train.to_numpy(dtype=int)
+    def __init__(self, x_train, y_train):
+        raw_x = np.asarray(x_train)
+        self.x_train = np.column_stack((raw_x, np.ones(raw_x.shape[0]))) 
+        
+        self.y_train = np.asarray(y_train, dtype=int).squeeze()
         
         self.num_classes = len(np.unique(self.y_train)) 
         
@@ -25,20 +26,15 @@ class SVM_Multiclass:
                 sigmas = (margins > 0).astype(int)
                 
                 # --- GRADIENT LOGIC ---
-                # The true class gradient is the negative sum of the rival violations
                 sigmas[label] = -np.sum(sigmas)
-                
-                # I want to scale X vector by each sigma, but I want gradients of same class to be columns
-                # so I transpose
                 gradient_matrix = np.outer(x_i, sigmas)
                 
                 # --- WEIGHT UPDATE (Pegasos style) ---
-                # Subtract learning_rate * (Gradient + L2 Regularization Penalty)
                 self.weights -= learning_rate * (gradient_matrix + (regularization * self.weights))
+                
             print(f"Epoch {epoch+1}/{epochs} completed .......")
 
-
     def predict(self, x_test):
-        x_test_np = x_test.to_numpy()
+        x_test_np = np.asarray(x_test)
         x_test_aug = np.column_stack((x_test_np, np.ones(x_test_np.shape[0])))
         return np.argmax(x_test_aug @ self.weights, axis=1)
